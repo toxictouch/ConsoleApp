0f43ab79509ae6727020ac92cd08c382c6923340
Stanley K. Munson III <ggmunson756@gmail.com>
2022-06-10 20:43:06 -0600

    updated gitignore

---
+++ ConsoleApp/App/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.App
+{
+    internal class Location
+    {
+        private static void TestMethod()
+        {
+            
+        }
+    }
+}
